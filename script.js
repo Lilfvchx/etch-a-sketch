6b5dda5d59e606297173b60d78084d5f38e056de
Lilfvchx <emiperalta256@gmail.com>
2022-03-20 07:13:10 -0300

    terminado solo falta darle estilo y hacerlo amigable con el user

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let size = Number(prompt("tamaño")) //pregunto mediante prompt al user el tamaño de cuadricula
+let size
+//genero funcion para que el usuario se vea obligado a colocar numero menor a 100
+const solicito = valordeseado=>{
+    if(valordeseado<100){
+        size=valordeseado
+        return
+    }
+    else {
+        solicito(prompt("dime valor maximo"))
+    }
+}
+solicito(prompt("dime valor maximo"))
 const divcentral = document.querySelector(".divcentral")
 let divgenerado
+let R= Math.floor(Math.random()*256)
+let G= Math.floor(Math.random()*256)
+let B= Math.floor(Math.random()*256)
 const borrargrid=document.querySelector(".borrargrid")
 //funcion para borrar grid
 function borrargrids() {
-    const botongenerado=
+    divcentral.innerHTML=""
+    solicito(prompt("dime valor maximo"))
 }
 //genero loop para que se creen los elementos y añadirlos al div central
 for (let index = 0; index < size * size; index++) {
@@ -18,12 +33,12 @@ divcentral.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 //cambiar clase de los divs que el mouse le pase encima para cambiar el color
  divcentral.addEventListener("mouseover", e=>{
     if (e.target.matches(".grid")) {
-        e.target.classList.add('active');
+        e.target.style.backgroundColor=`rgb(${R}, ${G}, ${B})`
       }
 })
  divcentral.addEventListener("mouseout", e=>{
      if(e.target.matches(".active")){
-        e.target.classList.remove("active")
+        e.target.style.backgroundColor="black"
      }
  })
  /////
